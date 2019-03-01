5deabcbe7886a0d9d8a403dabe5175c033f15fb9
Xavier La Rosa <xavier@Xaviers-MBP.home>
2019-03-01 00:25:01 -0500

    simplified home page functions

--- Nardwuar/Nardwuar/HomeViewController.swift
+++ Nardwuar/Nardwuar/HomeViewController.swift
@@ -13,42 +13,48 @@ class HomeViewController: UIViewController, UISearchBarDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupNavigationBarItems()
-        searchController.searchBar.showsScopeBar = true
-        searchController.searchBar.delegate = self
     }
-//making nav buttons global
-    let logoutButton = UIButton(type: .system)
-    let searchController = UISearchController(searchResultsController: nil)
-    let profileButton = UIButton(type: .system)
+//SETUP UI
     func setupNavigationBarItems(){
-        //logout button
+        setupLogoutButton()
+        setupSearchBar()
+        setupProfileButton()
+    }
+    
+//UI FUNCTIONS
+    let logoutButton = UIButton(type: .system)
+    func setupLogoutButton(){//LOGOUT
         logoutButton.setImage( UIImage(named: "icons8-cancel-50")?.withRenderingMode(.alwaysOriginal), for: .normal)
         logoutButton.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
         navigationItem.leftBarButtonItem = UIBarButtonItem(customView: logoutButton)
-         logoutButton.addTarget(self, action: #selector(handleLogoutSegue(sender:)), for: .touchUpInside)
-        
-        //searchbar
+        logoutButton.addTarget(self, action: #selector(handleLogoutSegue(sender:)), for: .touchUpInside)
+    }
+    
+    let searchController = UISearchController(searchResultsController: nil)
+    func setupSearchBar(){//SEARCHBAR
         navigationItem.searchController = searchController
         navigationItem.hidesSearchBarWhenScrolling = false
+        searchController.searchBar.showsScopeBar = true
+        searchController.searchBar.delegate = self
+        searchController.searchBar.tintColor = .white
         searchController.searchBar.setImage(UIImage(named: "icons8-music-100"), for: UISearchBar.Icon.search, state: .normal)
-//        searchController.searchBar.setImage(UIImage(named: "icons8-cancel-50"), for: UISearchBar.Icon.clear, state: .normal)
         UITextField.appearance(whenContainedInInstancesOf: [UISearchBar.self]).attributedPlaceholder = NSAttributedString(string: "Search for an Artist...", attributes: [NSAttributedString.Key.foregroundColor: UIColor.white])
+    }
     
-        //profile button
+    let profileButton = UIButton(type: .system)
+    func setupProfileButton(){//PROFILE
         profileButton.setImage( UIImage(named: "icons8-contacts-50")?.withRenderingMode(.alwaysOriginal), for: .normal)
         profileButton.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
         navigationItem.rightBarButtonItem = UIBarButtonItem(customView: profileButton)
         profileButton.addTarget(self, action: #selector(handleProfileSegue(sender:)), for: .touchUpInside)
-        
-        searchController.searchBar.tintColor = .white
     }
+    
+//SEGUES
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar){
         print("in searchbar clicked")
         searchBar.resignFirstResponder()
         performSegue(withIdentifier: "fromHomeToArtist", sender: self)
     }
-    
-    //handlers for segues
     @objc func handleProfileSegue(sender: UIButton){
         print("Profile segue method")
         performSegue(withIdentifier: "fromHomeToProfile", sender: self)
