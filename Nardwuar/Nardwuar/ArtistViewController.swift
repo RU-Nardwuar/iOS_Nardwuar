f06c0a54842506e1091a5fdf268fb4b3be15d165
Xavier La Rosa <xavier@Xaviers-MBP.home>
2019-03-14 01:40:09 -0400

    added some solid basic functionality for artist page such as where the api data will be held.
    added pictures too.

--- Nardwuar/Nardwuar/ArtistViewController.swift
+++ Nardwuar/Nardwuar/ArtistViewController.swift
@@ -10,20 +10,26 @@ import UIKit
 
 class ArtistViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if (section == 0){
-            return 3
-        }else if(section == 1){
-            return 2
-        }else if(section == 2){
-            return 1
-        }
-        return 1
+        return 4
     }
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 3
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150.00
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cellAPI", for: indexPath)
+        if(indexPath.row == 0){
+        cell.textLabel?.text = "Spotify API Details"
+        cell.imageView?.image = UIImage(named: "icons8-spotify-filled-50-3")
+        } else if(indexPath.row == 1){
+            cell.textLabel?.text = "Twitter API Details"
+            cell.imageView?.image = UIImage(named: "icons8-twitter-50-5")
+        } else if(indexPath.row == 2){
+            cell.textLabel?.text = "Genius API Details"
+            cell.imageView?.image = UIImage(named: "genius-50")
+        } else if(indexPath.row == 3){
+            cell.textLabel?.text = "Pitchfork API Details"
+            cell.imageView?.image = UIImage(named: "icons8-pitchfork-50")
+        }
         return cell
     }
     
@@ -58,6 +64,36 @@ class ArtistViewController: UIViewController, UITableViewDataSource, UITableView
         quickInfo.text = "Born February 28, 1990 in Los Angeles, California"
         
         backgroundImage.alpha = 0.85
+    }
+    
+}
+extension UIImage {
+    
+    func resize(maxWidthHeight : Double)-> UIImage? {
+        
+        let actualHeight = Double(size.height)
+        let actualWidth = Double(size.width)
+        var maxWidth = 0.0
+        var maxHeight = 0.0
+        
+        if actualWidth > actualHeight {
+            maxWidth = maxWidthHeight
+            let per = (100.0 * maxWidthHeight / actualWidth)
+            maxHeight = (actualHeight * per) / 100.0
+        }else{
+            maxHeight = maxWidthHeight
+            let per = (100.0 * maxWidthHeight / actualHeight)
+            maxWidth = (actualWidth * per) / 100.0
+        }
+        
+        let hasAlpha = true
+        let scale: CGFloat = 0.0
+        
+        UIGraphicsBeginImageContextWithOptions(CGSize(width: maxWidth, height: maxHeight), !hasAlpha, scale)
+        self.draw(in: CGRect(origin: .zero, size: CGSize(width: maxWidth, height: maxHeight)))
+        
+        let scaledImage = UIGraphicsGetImageFromCurrentImageContext()
+        return scaledImage
     }
     
 }
