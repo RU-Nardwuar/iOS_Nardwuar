442c19cdfbaec94950ec06616da138e2b5b94dd4
Xavier La Rosa <xavier@Xaviers-MBP.home>
2019-03-31 09:20:14 -0400

    made placeholders for artist page get and first five get

--- Nardwuar/Nardwuar/NetworkingClient.swift
+++ Nardwuar/Nardwuar/NetworkingClient.swift
@@ -13,7 +13,8 @@ class NetworkingClient{
     //https://jsonplaceholder.typicode.com/posts
     
     typealias WebServiceResponse = ([[String: Any]]?, Error?) -> Void
-    
+
+//GET REQUESTS
     //get request is working for accountData
     func GETaccountData(_ url: URL, completion: @escaping WebServiceResponse){
         Alamofire.request(url).validate().responseJSON { response in
@@ -27,4 +28,29 @@ class NetworkingClient{
         }
     }
     
+    //get request is working for artistData
+    func GETartistData(_ url: URL, completion: @escaping WebServiceResponse){
+        Alamofire.request(url).validate().responseJSON { response in
+            if let error = response.error {
+                completion(nil, error)
+            } else if let jsonArray = response.result.value as? [[String: Any]] {
+                completion(jsonArray, nil)
+            } else if let jsonDict = response.result.value as? [String : Any] {
+                completion([jsonDict], nil)
+            }
+        }
+    }
+    
+    //get request is working for first five artists
+    func GETfirstFiveArtistData(_ url: URL, completion: @escaping WebServiceResponse){
+        Alamofire.request(url).validate().responseJSON { response in
+            if let error = response.error {
+                completion(nil, error)
+            } else if let jsonArray = response.result.value as? [[String: Any]] {
+                completion(jsonArray, nil)
+            } else if let jsonDict = response.result.value as? [String : Any] {
+                completion([jsonDict], nil)
+            }
+        }
+    }
 }
