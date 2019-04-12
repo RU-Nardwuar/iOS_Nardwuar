d30374a3e43a77f1d073949c91887f24553dd38d
Xavier La Rosa <xavier@nbp-236-0.nbp.ruw.rutgers.edu>
2019-04-12 12:47:38 -0400

    trying to figure out error with artist connection

--- testAPIS.playground/Contents.swift
+++ testAPIS.playground/Contents.swift
@@ -6,58 +6,59 @@ import Foundation
 //////////////////////////////////// ARTIST JSON DATA ////////////////////////////////////////////////////////
 let ArtistJsonData = """
 {
-   "Pitchfork": [
-           {
-               "Album name": "KIDS SEE GHOSTS",
-               "Album description": "The psychic bond between Kanye West and Kid Cudi yields a spacious and melancholy album about brokenness—thoughts are fragmented, relationships are ended, and societal ties are cut.\n",
-               "Album score": 7.6,
-               "Album year": "2018",
-               "Best New Music": false,
-               "Label": "G.O.O.D. Music / Def Jam"
-           },
-           {
-               "Album name": "The Life Of Pablo",
-               "Album description": "Finally, after a protracted and often chaotic roll-out, the new Kanye West album is here. The Life of Pablo is the first Kanye West album that's just an album: No major statements, no reinventions, no zeitgeist wheelie-popping. But a madcap sense of humor animates all his best work, and the new record has a freewheeling energy that is infectious and unique to his discography.\n",
-               "Album score": 9,
-               "Album year": "2016",
-               "Best New Music": true,
-               "Label": "G.O.O.D. Music / Def Jam"
-           },
-           {
-               "Album name":"Ye",
-               "Album description": "Kanye West’s stint in Wyoming created an album born from chaos for chaos’ sake. Though it can be somewhat fascinating, it is undoubtedly a low point in his career.\n",
-               "Album score": 7.1,
-               "Album year": "2018",
-               "Best New Music": false,
-               "Label": "G.O.O.D. Music / Def Jam"
-           }
-   ],
-   "Spotify": {
-       "Albums": [
-       "KIDS SEE GHOSTS",
-       "ye",
-       "The Life Of Pablo",
-       "Yeezus",
-       "Kanye West Presents Good Music Cruel Summer",
-       "My Beautiful Dark Twisted Fantasy",
-       "808s & Heartbreak",
-       "Graduation (French Limited Version)",
-       "Graduation",
-       "Graduation (Exclusive Edition)",
-       "Graduation (Alternative Business Partners)",
-       "Late Orchestration",
-       "Late Registration"
-       ],
-       "Artist Name": "Kanye West",
-       "Genres": [
-       "chicago rap",
-       "pop rap",
-       "rap"
-       ],
-       "Total Number of Spotify Followers": 9533117
-   }
+    "Pitchfork": [
+        {
+            "Album description": "The psychic bond between Kanye West and Kid Cudi yields a spacious and melancholy album about brokenness—thoughts are fragmented, relationships are ended, and societal ties are cut.\n",
+            "Album name": "KIDS SEE GHOSTS",
+            "Album score": 7.6,
+            "Album year": "2018",
+            "Best New Music": false,
+            "Label": "G.O.O.D. Music / Def Jam"
+        },
+        {
+            "Album description": "Kanye West’s stint in Wyoming created an album born from chaos for chaos’ sake. Though it can be somewhat fascinating, it is undoubtedly a low point in his career.\n",
+            "Album name": "ye",
+            "Album score": 7.1,
+            "Album year": "2018",
+            "Best New Music": false,
+            "Label": "G.O.O.D. Music / Def Jam"
+        },
+        {
+            "Album description": "Finally, after a protracted and often chaotic roll-out, the new Kanye West album is here. The Life of Pablo is the first Kanye West album that's just an album: No major statements, no reinventions, no zeitgeist wheelie-popping. But a madcap sense of humor animates all his best work, and the new record has a freewheeling energy that is infectious and unique to his discography.\n",
+            "Album name": "The Life Of Pablo",
+            "Album score": 9,
+            "Album year": "2016",
+            "Best New Music": true,
+            "Label": "G.O.O.D. Music / Def Jam"
+        }
+    ],
+    "Spotify": {
+        "Albums": [
+            "KIDS SEE GHOSTS",
+            "ye",
+            "The Life Of Pablo",
+            "Yeezus",
+            "Kanye West Presents Good Music Cruel Summer",
+            "My Beautiful Dark Twisted Fantasy",
+            "808s & Heartbreak",
+            "Graduation (French Limited Version)",
+            "Graduation",
+            "Graduation (Exclusive Edition)",
+            "Graduation (Alternative Business Partners)",
+            "Late Orchestration",
+            "Late Registration"
+        ],
+        "Artist Name": "Kanye West",
+        "Genres": [
+            "chicago rap",
+            "pop rap",
+            "rap"
+        ],
+        "Total Number of Spotify Followers": 9535222
+    }
 }
 """.data(using: .utf8)!
+
 struct Artist: Codable {
     let pitchfork: [Pitchfork]
     let spotify: Spotify
@@ -69,15 +70,15 @@ struct Artist: Codable {
 }
 
 struct Pitchfork: Codable {
-    let albumName, albumDescription: String
+    let albumDescription, albumName: String
     let albumScore: Double
     let albumYear: String
     let bestNewMusic: Bool
     let label: String
     
     enum CodingKeys: String, CodingKey {
-        case albumName = "Album name"
         case albumDescription = "Album description"
+        case albumName = "Album name"
         case albumScore = "Album score"
         case albumYear = "Album year"
         case bestNewMusic = "Best New Music"
@@ -100,8 +101,8 @@ struct Spotify: Codable {
 }
 
 let artist = try? JSONDecoder().decode(Artist.self, from: ArtistJsonData)
-print(artist!.pitchfork[0] as Any) //change to artist? after implementation
-print(artist!.spotify as Any)
+print(artist?.pitchfork[0] as Any) //change to artist? after implementation
+print(artist?.spotify as Any)
 
 
 //////////////////////////////////// USER JSON DATA ////////////////////////////////////////////////////////
