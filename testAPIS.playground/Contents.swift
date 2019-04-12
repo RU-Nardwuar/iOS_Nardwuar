0f63f6f84acac086582b686983b9515bf5063667
Xavier La Rosa <xavier@nbp-236-0.nbp.ruw.rutgers.edu>
2019-04-12 12:40:48 -0400

    fixed users json data, artist json data is still being a little weird

--- testAPIS.playground/Contents.swift
+++ testAPIS.playground/Contents.swift
@@ -5,7 +5,6 @@ import Foundation
 
 //////////////////////////////////// ARTIST JSON DATA ////////////////////////////////////////////////////////
 let ArtistJsonData = """
-{[
 {
    "Pitchfork": [
            {
@@ -58,11 +57,7 @@ let ArtistJsonData = """
        "Total Number of Spotify Followers": 9533117
    }
 }
-]}
 """.data(using: .utf8)!
-
-import Foundation
-
 struct Artist: Codable {
     let pitchfork: [Pitchfork]
     let spotify: Spotify
@@ -103,13 +98,14 @@ struct Spotify: Codable {
         case totalNumberOfSpotifyFollowers = "Total Number of Spotify Followers"
     }
 }
+
 let artist = try? JSONDecoder().decode(Artist.self, from: ArtistJsonData)
-print(artist as Any)
+print(artist!.pitchfork[0] as Any) //change to artist? after implementation
+print(artist!.spotify as Any)
 
 
 //////////////////////////////////// USER JSON DATA ////////////////////////////////////////////////////////
 let userJSONData = """
-{[
     {
     "id_token":"eyJhbGciOiJSUzI1NiIsImtpZCI6ImZmMWRmNWExNWI1Y2Y1ODJiNjFhMjEzODVjMGNmYWVkZmRiNmE3NDgiLCJ0eXAiOiJKV1QifQ.eyJuYW1lIjoiWGF2aWVyIExhIFJvc2EiLCJwaWN0dXJlIjoiaHR0cHM6Ly9saDYuZ29vZ2xldXNlcmNvbnRlbnQuY29tLy1EX0FVYW5WaWdZRS9BQUFBQUFBQUFBSS9BQUFBQUFBQUFBQS9BQ0hpM3JlZzN0cGJHbF81T1ppdHBYOWtjZkROT1RpejVBL3M5Ni1jL3Bob3RvLmpwZyIsImlzcyI6Imh0dHBzOi8vc2VjdXJldG9rZW4uZ29vZ2xlLmNvbS9uYXJkd3Vhci03ZTZmYyIsImF1ZCI6Im5hcmR3dWFyLTdlNmZjIiwiYXV0aF90aW1lIjoxNTU0MDQyMzcyLCJ1c2VyX2lkIjoiVTY1QkljdW80T1pySWIxMGxaQ2tUdjY1Y0Q0MiIsInN1YiI6IlU2NUJJY3VvNE9ackliMTBsWkNrVHY2NWNENDIiLCJpYXQiOjE1NTQwNDIzNzMsImV4cCI6MTU1NDA0NTk3MywiZW1haWwiOiJsYXJvc2EueGF2aWVyQHN0dWRlbnQuY2NtLmVkdSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJmaXJlYmFzZSI6eyJpZGVudGl0aWVzIjp7Imdvb2dsZS5jb20iOlsiMTA3OTQ0MjE4MjQ5NjE3NDc2MTc3Il0sImVtYWlsIjpbImxhcm9zYS54YXZpZXJAc3R1ZGVudC5jY20uZWR1Il19LCJzaWduX2luX3Byb3ZpZGVyIjoiZ29vZ2xlLmNvbSJ9fQ.eWk-_FjZexQ0q_5RruM_PenIXUWpZKX4OtnOkbeiEEwAd5J4eeHD7f_f-Xs0siONA4bUGlD9CZkcklS_QqEEw6VmfwX_iCuthc2F6LPEIPDTSdgeuwPK6e3_fmU31kRhyJeIs2qZPs5L__w3yOXB0tVWpT4XCsRFAXX-w00b9cCKTQHohG9o-4TMtmM_sZLHrZsahwqyH5XKeoTfq3_2nggBVwmPGfAc71FJbm_uy5Ag6XWVR01JMpEGRQAbp1w9qtK0eXbIDWlfqCdQXLtt2lizgND43w1t4U4lotkjDCHYHRgpB99etxvbpPtWKkvEGJ4Iw3Jd2ao06FUHenOXUQ",
         "name":"Xavier",
@@ -121,7 +117,6 @@ let userJSONData = """
             }
         ]
     }
-]}
 """.data(using: .utf8)!
 
 struct User: Codable {
@@ -143,4 +138,7 @@ struct Following: Codable {
     }
 }
 let user = try? JSONDecoder().decode(User.self, from: userJSONData)
-print(user as Any)
+print(user!.following[0] as Any)
+print(user!.idToken as Any)
+print(user!.name as Any)
+print(user!.username as Any) // make them user? when implementing
